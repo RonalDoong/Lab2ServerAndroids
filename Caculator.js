2d7d18febeca4f928bb299848c042bd496651972
RonalDoong <127843122+RonalDoong@users.noreply.github.com>
2023-07-10 10:32:45 +0700

    Add files via upload

---
+++ Caculator.js
@@ -0,0 +1,14 @@
+var exports = module.exports;
+ 
+exports.tong = function(a,b){
+    return a + b;
+}
+exports.hieu = function(a,b){
+    return a - b;
+}
+exports.tich = function(a,b){
+    return a * b;
+}
+exports.thuong = function(a,b){
+    return a / b;
+}
---
+++ server.js
@@ -0,0 +1,7 @@
+var _cal = require('./Caculator')
+var a = 20;
+var b = 5;
+console.log('tong: '+_cal.tong(a,b));
+console.log('hieu: '+_cal.hieu(a,b));
+console.log('tich: '+_cal.tich(a,b));
+console.log('thuong: '+_cal.thuong(a,b));
